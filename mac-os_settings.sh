133be90f4227835fad658e277b54c94caadad6cc
yhkaplan <yhkaplan@gmail.com>
2018-01-16 10:41:53 +0900

    Additional setting idea

--- mac-os_settings.sh
+++ mac-os_settings.sh
@@ -2,5 +2,8 @@
 #make Finder show list of files by default instead of icons
 # To add
 
+#Show date in menu bar alongside time
+# To add
+
 # Put dock on the left
 defaults write com.apple.dock orientation -string left
