a2c363b39ab041385ecf7b4c725d62ae7e42c329
Joshua Kaplan <yhkaplan@gmail.com>
2018-01-20 23:56:09 +0900

    Swift support added

--- fish/config.fish
+++ fish/config.fish
@@ -9,9 +9,12 @@ set -g -x PATH /usr/local/bin $PATH
 # Gotham Shell
 #eval sh $HOME/.config/gotham/gotham.sh
 
+# For Swift
+set PATH /usr/bin/ $PATH
+#set PATH /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ $PATH
+
 # For Golang
-export GOPATH=$HOME/go/
-# set -xg GOPATH $HOME/go
+set -x GOPATH $HOME/go/
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
