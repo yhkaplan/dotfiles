1732ed6e310c25ec91eac7da1ce0cb5ae5d8f126
yhkaplan <yhkaplan@gmail.com>
2018-01-23 20:42:22 +0900

    Added themes

--- install-themes.sh
+++ install-themes.sh
@@ -9,6 +9,14 @@ git clone 'https://github.com/whatyouhide/gotham-contrib.git'
 
 git clone 'https://github.com/iest/Panic-Palette-Extras.git'
 
+git clone 'https://github.com/cocopon/iceberg.vim.git'
+git clone 'https://github.com/cocopon/xcode-iceberg.git'
+# iTerm 2 https://github.com/aseom/dotfiles/blob/master/osx/iterm2/iceberg.itermcolors
+
+git clone 'https://github.com/bojan/xcode-one-dark.git'
+git clone 'https://github.com/sonph/onehalf.git'
+git clone 'https://github.com/PatMurrayDEV/San-Jose.git'
+
 git clone 'https://github.com/dracula/alfred'
 git clone 'https://github.com/dracula/vim.git'
 git clone 'https://github.com/dracula/terminal.app.git'
