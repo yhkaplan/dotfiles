7edf3dc174f9ba39e26990379a12f431664d7971
yhkaplan <yhkaplan@gmail.com>
2018-02-07 12:53:32 +0900

    Added many abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -17,5 +17,34 @@ set -x GOROOT /usr/local/opt/go/libexec
 set -g -x PATH $GOPATH/bin $PATH
 set -g -x PATH $GOROOT/bin $PATH
 
+# For iTerm2
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
+############# ABBREVIATIONS #################
+
+if status --is-interactive
+    set -g fish_user_abbreviations
+    
+    # Git-related abbreviations
+    abbr -a gpl git pull
+    abbr -a gps git push
+    abbr -a ga git add -A
+    abbr -a gc git commit -m "
+    abbr -a gco git checkout
+    abbr -a gs git status -s
+    abbr -a gf git fetch
+    abbr -a gb git branch
+    abbr -a gm git merge
+    abbr -a gd git diff
+    abbr -a gl git log -1 HEAD
+    abbr -a gsl git log -8 --pretty --oneline
+    abbr -a ggui git log --graph --decorate --oneline
+    abbr -a gdel git branch -d
+    abbr -a gclean git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d
+
+    # Other abbreviations
+    abbr -a ow open *.xcworkspace/
+    abbr -a nv nvim
+    abbr -a trel tree -L 2
+
+end
