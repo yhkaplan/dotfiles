30c7d1a2fcd575cb84421f0b74f40f1d22a5f929
yhkaplan <yhkaplan@gmail.com>
2018-08-21 19:10:36 +0900

    Move FZF settings to config.fish so they actually take affect

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,14 @@ and not set -q TMUX
     exec tmux
 end
 
+# FZF
+# Runs a tmux-friendly version of fzf instead.
+set -U FZF_TMUX 1
+# Use fd for better performance, skip .git, show hidden files, and follow symlinks
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 # Sets path for things installed w/ Homebrew
 set -g -x PATH /usr/local/bin $PATH
 
--- post_bootstrap.sh
+++ post_bootstrap.sh
@@ -4,9 +4,3 @@ curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fishe
 fisher install tuvistavie/fish-ssh-agent
 fisher install fzf
 fisher install eden
-# Runs a tmux-friendly version of fzf instead.
-set -U FZF_TMUX 1
-# Use fd for better performance, skip .git, show hidden files, and follow symlinks
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-# To apply the command to CTRL-T as well
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
