ddaee50cbfa88ca71e49f37accb41c221f801124
Joshua Kaplan <yhkaplan@gmail.com>
2018-08-29 22:34:20 +0900

    Remove ignore storyboard for now

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,7 @@ end
 # Runs a tmux-friendly version of fzf instead.
 set -U FZF_TMUX 1
 # Use fd for better performance, skip .git, show hidden files, and follow symlinks
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --exclude **.storyboard'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
