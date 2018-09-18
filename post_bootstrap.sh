d11de673421ec38cecbaa192b0712e17ff709b33
yhkaplan <yhkaplan@gmail.com>
2018-09-18 14:01:47 +0900

    Delete post_boostrap

--- post_bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/fish
-
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-fisher install tuvistavie/fish-ssh-agent
-fisher install fzf
-fisher install eden
