0a0a6001a04f2018ba0efef9c7ff77288cd789b1
yhkaplan <yhkaplan@gmail.com>
2018-11-12 19:27:02 +0900

    Remove fish related files, completing the switch to zsh

--- fish/fish_user_key_bindings.fish
+++
@@ -1,8 +0,0 @@
-function fish_user_key_bindings
-    fzf_key_bindings
-end
-
-bind \cg '__ghq_cd_repository'
-if bind -M insert >/dev/null ^/dev/null
-  bind -M insert \cg '__ghq_cd_repository'
-end
--- fish/git.fish
+++
@@ -1,5 +0,0 @@
-#Wraps git with hub functions
-
-function git --description 'Alias for hub that wraps git to provide extra functionality with GitHub'
-    hub $argv
-end
