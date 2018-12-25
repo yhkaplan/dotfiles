fd1e7af49bd5b4b66c93372e54471447b8205be2
yhkaplan <yhkaplan@gmail.com>
2018-12-25 18:40:09 +0900

    Change key repeat settings

--- mac-os_settings.sh
+++ mac-os_settings.sh
@@ -46,8 +46,8 @@ defaults write com.apple.dock orientation -string left
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+# defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write NSGlobalDomain InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 
 ###############################################################################
 # Xcode                                                                       #
