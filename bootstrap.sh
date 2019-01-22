58a10c3a0f42880b4fa4348e3fbc43958618eb40
yhkaplan <yhkaplan@gmail.com>
2019-01-22 18:53:56 +0900

    shfmt bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,11 @@
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+done 2>/dev/null &
 
 echo "Starting bootstrapping"
 
@@ -26,7 +30,8 @@ echo "Installing packages..."
 # Select directory and run brewfile
 # The Brewfile is generated automatically through 'brew bundle dump'
 mv ~/dotfiles ~/.dotfiles
-cd ~/.dotfiles/ || echo "Could not cd dotfiles"; exit 1
+cd ~/.dotfiles/ || echo "Could not cd dotfiles"
+exit 1
 brew bundle
 brew cleanup
 brew cask cleanup
@@ -37,7 +42,7 @@ brew cask cleanup
 echo "Setting up Ruby. Please make sure to check that the version of bundler matches the version installed by rbenv"
 
 LINE='eval "$(rbenv init -)"'
-grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra echo "Cleaning up..."
+grep -q "$LINE" ~/.extra || echo "$LINE" echo "Cleaning up..." >>~/.extra
 
 echo "Installing Ruby versions"
 rbenv install 2.5.1
@@ -51,7 +56,7 @@ sudo gem install bundler xcpretty
 ########## Other ################
 
 # Zsh plugins
-antibody bundle < ~/.dotfiles/zsh/.zsh_plugins.txt > ~/.zsh_plugins.sh
+antibody bundle <~/.dotfiles/zsh/.zsh_plugins.txt >~/.zsh_plugins.sh
 
 # Dependencies for Deoplete on other Pythonic stuff
 pip3 install -r pip-requirements.txt
@@ -92,13 +97,16 @@ sudo bash -c 'echo /usr/local/bin/zsh >> /etc/shells'
 while true; do
     read -r -p "Do you want Zsh to be your default shell? " yn
     case $yn in
-        [Yy]* )
-            chsh -s /usr/local/bin/zsh
-            echo "Bootstrapping complete"
-            zsh
-            break;;
-        [Nn]* ) echo "Bootstrapping complete"; exit;;
-        * ) echo "Please input yes or no";;
+    [Yy]*)
+        chsh -s /usr/local/bin/zsh
+        echo "Bootstrapping complete"
+        zsh
+        break
+        ;;
+    [Nn]*)
+        echo "Bootstrapping complete"
+        exit
+        ;;
+    *) echo "Please input yes or no" ;;
     esac
 done
-
