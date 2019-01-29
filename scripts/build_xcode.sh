11cf96c43068a970c52458481886bfbdf2b09398
yhkaplan <yhkaplan@gmail.com>
2019-01-29 12:11:09 +0900

    Make script switch back to iTerm after starting build

--- scripts/build_xcode.sh
+++ scripts/build_xcode.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 osascript -e 'tell application "Xcode"
-    activate
+  activate
 
-    set targetProject to active workspace document
-    if (build targetProject) is equal to "Build succeeded" then
-        launch targetProject
-    end if
-end tell'
+  set targetProject to active workspace document
+  if (build targetProject) is equal to "Build succeeded" then
+    launch targetProject
+  end if
+end tell
+
+tell application "iTerm"
+  activate
+end tell
+'
