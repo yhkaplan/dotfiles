6c533579b3098a05bd69bf65582d7d4392fdb00e
yhkaplan <yhkaplan@gmail.com>
2019-03-14 12:13:17 +0900

    Add more macOS defaults

--- mac-os_settings.sh
+++ mac-os_settings.sh
@@ -7,12 +7,20 @@ osascript -e 'tell application "System Preferences" to quit'
 # Ask for the administrator password upfront
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 #make Finder show list of files by default instead of icons
 # To add
 
+# Show language menu in the top right corner of the boot screen
+sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
+
 # Make battery indicator show percent remaining
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-# Make safari open tabs last opened
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
 
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
@@ -26,6 +34,15 @@ defaults write com.apple.Finder FXPreferredViewStyle clmv
 
 # Finder: show path bar
 defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
 
 # Turn on app auto-update
 defaults write com.apple.commerce AutoUpdate -bool true
@@ -55,6 +72,16 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 10 # normal minimum is 15 (2
 
 # Display build times
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration -bool YES
+
+# Add iOS Simulator to Launchpad
+sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app" "/Applications/Simulator.app"
+
+###############################################################################
+# Spectacle.app                                                               #
+###############################################################################
+
+# Set up my preferred keyboard shortcuts
+# cp -r init/spectacle.json ~/Library/Application\ Support/Spectacle/Shortcuts.json 2> /dev/null
 
 ###############################################################################
 # Kill affected applications                                                  #
